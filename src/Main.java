a27d33fda7387b6ab949de28db39c46167ba6e54
Uriel Barbosa <uprieto@gmail.com>
2024-04-30 20:20:16 -0300

    new music instance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import com.uriamp.models.Music;
+
 public class Main {
   public static void main(String[] args) {
-    
+    Music music1 = new Music();
+
+    music1.setArtist("Metallica");
+    music1.setAlbum("Master of Puppets");
+    music1.setTitle("Damage Inc.");
+    music1.setGenre("Metal");
+
+    System.out.println(music1.getAlbum());
+
   }
 }
--- src/com/uriamp/models/Podcast.java
+++ src/com/uriamp/models/Podcast.java
@@ -1,5 +1,23 @@
 package com.uriamp.models;
 
 public class Podcast extends AudioFile {
+  private String host;
+  private String preview;
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public String getPreview() {
+    return preview;
+  }
+
+  public void setPreview(String preview) {
+    this.preview = preview;
+  }
 
 }
